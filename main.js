bbe1e8a19789750adb67d62aeeaea87ba7fc76e7
lishuyu69 <748983245@qq.com>
2020-10-02 10:31:00 +0800

    test

---
+++ main.js
@@ -0,0 +1,17 @@
+var xmlhttp;
+function setUrl(urlstr) {
+    var url = "" + urlstr;
+    history.pushState({ url: url, title: document.title }, document.title, url);
+}
+function loadContent(urlstr) {
+    var url = "" + urlstr;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = contentLoaded;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(null);
+}
+function contentLoaded() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        document.getElementById('main').innerHTML = xmlhttp.responseText;
+    }
+}
