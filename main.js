48210eb77423e129acecb012877e45263ddbffb4
lishuyu69 <748983245@qq.com>
2020-10-02 11:22:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function loadContent(urlstr) {
 }
 function contentLoaded() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        document.getElementById('main').innerHTML = xmlhttp.responseText;
         document.write('<scr' + 'ipt defer src="https://code.getmdl.io/1.3.0/material.min.js"></scr' + 'ipt>');
+        document.getElementById('main').innerHTML = xmlhttp.responseText;
     }
 }
 if ((location + "").match("index.html") == "index.html") {
