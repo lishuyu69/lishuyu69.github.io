5771ada1b57fa7f1999e2638e68a48675d388a3e
lishuyu69 <748983245@qq.com>
2020-10-02 17:39:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function contentLoaded() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         /*document.getElementById('main').innerHTML = null;*/
         /*document.getElementById('head').innerHTML = "";*/
-        document.getElementById('body').innerHTML = '<script defer="defer" async="async" src="https://code.getmdl.io/1.3.0/material.min.js"></script>' + xmlhttp.responseText;
+        document.getElementById('main').innerHTML = '<script defer="defer" async="async" src="https://code.getmdl.io/1.3.0/material.min.js"></script>' + xmlhttp.responseText;
         /*document.getElementById('head').innerHTML = document.getElementById('head').innerHTML + ('<scr' + 'ipt defer src="https://code.getmdl.io/1.3.0/material.min.js"></scr' + 'ipt>');*/
     }
 }
