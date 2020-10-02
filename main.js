ace6b978df77058ce7eeb9eea922cefc9ed234c3
lishuyu69 <748983245@qq.com>
2020-10-02 14:05:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function loadContent(urlstr) {
 function contentLoaded() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         document.getElementById('main').innerHTML = null;
-        document.write('<scr' + 'ipt defer src="https://code.getmdl.io/1.3.0/material.min.js"></scr' + 'ipt>');
         document.write(xmlhttp.responseText);
+        document.getElementById('head') += write('<scr' + 'ipt defer src="https://code.getmdl.io/1.3.0/material.min.js"></scr' + 'ipt>');
     }
 }
 if ((location + "").match("index.html") == "index.html") {
