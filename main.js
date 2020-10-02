cd271e98172e4e72e15fd7f5fabcc089e15ddc92
lishuyu69 <748983245@qq.com>
2020-10-02 11:10:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ function loadContent(urlstr) {
 }
 function contentLoaded() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        document.getElementById('main').innerHTML = xmlhttp.responseText+'<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>';
+        document.getElementById('main').innerHTML = xmlhttp.responseText;
+        document.write('<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>');
     }
 }
 if ((location + "").match("index.html") == "index.html") {
