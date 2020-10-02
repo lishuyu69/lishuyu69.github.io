ec85bb06270974e149001ccfe2c371d96518d1c4
lishuyu69 <748983245@qq.com>
2020-10-02 14:34:14 +0800

    test

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function loadContent(urlstr) {
 function contentLoaded() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         /*document.getElementById('main').innerHTML = null;*/
+        document.write(<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>'')
         document.getElementById('head').innerHTML = xmlhttp.responseText;
         /*document.getElementById('head').innerHTML = document.getElementById('head').innerHTML + ('<scr' + 'ipt defer src="https://code.getmdl.io/1.3.0/material.min.js"></scr' + 'ipt>');*/
     }
